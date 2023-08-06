4a2ce65812a667db706c0205d1beb91d50ffb7a1
Y.S .Jenifer Singh <30699751+Jenifersingh@users.noreply.github.com>
2023-08-06 13:18:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,49 +3,46 @@
  */
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      // Get content from the network.
-      // try {
-      const url = new URL(event.request.url);
-      return await fetch(event.request);
-      console.log("URL", url);
-      if (
-        event.request.method === "POST" &&
-        url.pathname === "/share_target/sharetarget.html"
-      ) {
-        console.log("INSIDE URL", event.request);
-        const formData = await event.request.formData();
-
-        self.clients.matchAll({ type: "window" }).then((clients) => {
-          clients.forEach((client) => {
-            client.postMessage({
-              formData,
-              id: "IDDS",
-            });
-          });
-        });
-      } else {
-        return await fetch(event.request);
-      }
-      // } catch (e) {
-      //   // Failure. Just return a 200 page, to satisfy Lighthouse.
-      //   console.log(e);
-      //   self.clients.matchAll({ type: "window" }).then((clients) => {
-      //     clients.forEach((client) => {
-      //       client?.postMessage({
-      //         e,
-      //         id: "IDDS",
-      //       });
-      //     });
-      //   });
-
-      //   return await fetch(event.request);
-
-      //   // return new Response("You are offline :(", { status: 200, e });
-      // }
-    })()
-  );
+  // event.respondWith(
+  //   (async () => {
+  //     // Get content from the network.
+  //     // try {
+  //     const url = new URL(event.request.url);
+  //     return await fetch(event.request);
+  //     console.log("URL", url);
+  //     if (
+  //       event.request.method === "POST" &&
+  //       url.pathname === "/share_target/sharetarget.html"
+  //     ) {
+  //       console.log("INSIDE URL", event.request);
+  //       const formData = await event.request.formData();
+  //       self.clients.matchAll({ type: "window" }).then((clients) => {
+  //         clients.forEach((client) => {
+  //           client.postMessage({
+  //             formData,
+  //             id: "IDDS",
+  //           });
+  //         });
+  //       });
+  //     } else {
+  //       return await fetch(event.request);
+  //     }
+  //     // } catch (e) {
+  //     //   // Failure. Just return a 200 page, to satisfy Lighthouse.
+  //     //   console.log(e);
+  //     //   self.clients.matchAll({ type: "window" }).then((clients) => {
+  //     //     clients.forEach((client) => {
+  //     //       client?.postMessage({
+  //     //         e,
+  //     //         id: "IDDS",
+  //     //       });
+  //     //     });
+  //     //   });
+  //     //   return await fetch(event.request);
+  //     //   // return new Response("You are offline :(", { status: 200, e });
+  //     // }
+  //   })()
+  // );
 });
 
 // self.addEventListener('fetch', event => {
