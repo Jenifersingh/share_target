540ea80c75510babf5d3a6fd9acc1a1605d8e1ca
Y.S .Jenifer Singh <30699751+Jenifersingh@users.noreply.github.com>
2023-08-06 19:49:41 +0530

    Update sw.js - removed cache

--- sw.js
+++ sw.js
@@ -6,32 +6,26 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
       // Get content from the network.
-      try {
-        const url = new URL(event.request.url);
-        console.log("URL", url);
-        if (
-          event.request.method === "POST" &&
-          url.pathname === "/share_target/sharetarget.html"
-        ) {
-          console.log("INSIDE URL", event.request);
-          const formData = await event.request.formData();
-          self.clients.matchAll({ type: "window" }).then((clients) => {
-            clients.forEach((client) => {
-              client.postMessage({
-                formData,
-                id: "IDDS",
-              });
+
+      const url = new URL(event.request.url);
+      console.log("URL", url);
+      if (
+        event.request.method === "POST" &&
+        url.pathname === "/share_target/sharetarget.html"
+      ) {
+        console.log("INSIDE URL", event.request);
+        const formData = await event.request.formData();
+        self.clients.matchAll({ type: "window" }).then((clients) => {
+          clients.forEach((client) => {
+            client.postMessage({
+              formData,
+              id: "IDDS",
             });
           });
-        }
-
-        Response.redirect(url);
-
-        // return await fetch(event.request);
-      } catch (e) {
-        // Failure. Just return a 200 page, to satisfy Lighthouse.
-        return new Response("You are offline :(", { status: 200 });
+        });
       }
+
+      Response.redirect(url);
     })()
   );
 });
